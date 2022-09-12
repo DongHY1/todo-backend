8dd4224a77ac72a7c37a18501a8174d7337acf78
haoyudong <2070343054@email.szu.edu.cn>
2022-09-12 23:39:13 +0800

    get todos from database

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const pool = require('./db.js')
 const app = express();
 const port = 5001;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('hello world');
+app.get('/todos', async (req, res) => {
+ try {
+  const allTodos = await pool.query("SELECT * FROM todo ")
+  res.json(allTodos.rows)
+ } catch (error) {
+  console.error(error.message)
+ }
 });
+
 app.listen(port, () => {
   console.log('5001 is listen!');
 });
