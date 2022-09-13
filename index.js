ad54d6900a2a08560499569fcc764fb3927ab3f8
haoyudong <2070343054@email.szu.edu.cn>
2022-09-13 23:15:59 +0800

    add POST todo

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const cors = require('cors');
-const pool = require('./db.js')
+const pool = require('./db.js');
 const app = express();
 const port = 5001;
 app.use(cors());
 app.use(express.json());
 
 app.get('/todos', async (req, res) => {
- try {
-  const allTodos = await pool.query("SELECT * FROM todo ")
-  res.json(allTodos.rows)
- } catch (error) {
-  console.error(error.message)
- }
+  try {
+    const allTodos = await pool.query('SELECT * FROM todo ');
+    res.json(allTodos.rows);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+app.post('/todos', async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      'INSERT INTO todo (description) VALUES($1) RETURNING *',
+      [description]
+    );
+    res.json(newTodo.rows[0])
+  } catch (error) {
+    console.error(error.message);
+  }
 });
 
 app.listen(port, () => {
